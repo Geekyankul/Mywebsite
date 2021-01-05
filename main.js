906fbc5102709565d47fd6525f57ddfab6cd7859
Geekyankul <Ankul.cse.united@gmail.com>
2021-01-05 12:36:21 +0530

    modified calc

---
+++ main.js
@@ -0,0 +1,34 @@
+// document.write("<h1>External javascript</h1>");
+
+// let ankul={
+//     id:12,
+//     name:"Ankul",
+//     salary:2121,
+//     isfemale:true,
+//     gender:{
+//         f: "female",
+//         m: "male"
+
+//     }
+// };
+// // function checkgender(obj){
+// //     if (obj.isfemale){
+// //         return obj.gender.f;
+// //     }else{
+// //         return obj.gender.m;
+// //     }
+// // }
+// // document.write(ankul.id +" "+ankul.name +" "+ ankul.salary +" "+checkgender(ankul))
+
+
+
+// let mycar= ["Volvo","BMW","Audi","Honda","suzuki"];
+// //document.write(mycar.length);
+// mycar.push("tesla");
+// mycar.shift();
+// for(let i=0; i<mycar.length; i++)
+// {
+//  document.write(mycar[i] +"<br>");
+// }
+
+
